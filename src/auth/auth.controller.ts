e9ea9e0ca46ab2271c30fde8ba811de594e3c63a
Muhammad Usama <underusama@gmail.com>
2022-10-09 06:26:36 -0700

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
+import { SignupDto, LoginDto } from './dto';
+import { JwtGuard } from './guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup(@Body() dto: SignupDto) {
+    return this.authService.signup(dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @Get('secret')
+  @UseGuards(JwtGuard)
+  secret(@Req() req: Request) {
+    console.log(req.user);
+    return { message: 'Super Secret' };
+  }
+}
